43017e16ee2a6bbf38418a3b1748b7485eeae238
Sudarshan Acharya <info@sacharya.com>
2015-06-22 06:23:07 +0000

    Changing image name to ubuntu-swiftly

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 file_full_path=$1
 filename=$(basename "$1")
 echo $filename
-sudo docker build -t suda-swiftly .
+sudo docker build -t ubuntu-swiftly .
 
 containername="tweet-docker-swiftly"
-sudo docker run -i -t suda-swiftly swiftly put $containername
-sudo docker run -i -t -v /tmp/uploads:/tmp suda-swiftly swiftly put -i /tmp/$filename $containername/$filename
+sudo docker run -i -t ubuntu-swiftly swiftly put $containername
+sudo docker run -i -t -v /tmp/uploads:/tmp ubuntu-swiftly swiftly put -i /tmp/$filename $containername/$filename
